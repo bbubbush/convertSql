ffb0d8e1f5225141ecd4a5fe9132c8bfe984bb80
bbubbush <bbubbush@gmail.com>
2020-05-06 21:27:39 +0900

    커커밋

---
+++ src/test/java/com/bbubbush/convert/ConvertApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bbubbush.convert;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
